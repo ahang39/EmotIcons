9ac32bfa35bfd6106577f359951e64280e40bf50
joyme <786185931@qq.com>
2016-04-30 12:47:27 +0800

    调整代码结构，module中存放各个模块的子页面 个人中心结构完成

--- js/personalCenter.js
+++ js/personalCenter.js
@@ -5,27 +5,33 @@ mui.plusReady(function(){
 			}
 		}, "警告", ["是", "否"]);
 	});
+	mui("#profile_panel").on('tap',"#profile_setting",function(){
+		mui.openWindow({
+			url:"module/personalCenter/profile.html",
+			id:"profile.html"
+		});
+	});
 	mui("#setting_panel").on('tap',"#notification_setting",function(){
 		mui.openWindow({
-			url:"notification_setting.html",
+			url:"module/personalCenter/notification_setting.html",
 			id:"notification_setting.html"
 		});
 	});
 	mui("#setting_panel").on('tap',"#privacy_setting",function(){
 		mui.openWindow({
-			url:"privicy_setting.html",
+			url:"module/personalCenter/privacy_setting.html",
 			id:"privacy_setting.html"
 		});
 	});
 	mui("#setting_panel").on('tap',"#common_setting",function(){
 		mui.openWindow({
-			url:"common_setting.html",
+			url:"module/personalCenter/common_setting.html",
 			id:"common_setting.html"
 		});
 	});
 	mui("#about_panel").on('tap',"#about",function(){
 		mui.openWindow({
-			url:"about.html",
+			url:"module/personalCenter/about.html",
 			id:"about.html"
 		});
 	});
