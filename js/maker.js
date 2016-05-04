b509ae605e9e1f027c76fea7ae9596c5a0ce7d88
joyme <786185931@qq.com>
2016-05-04 23:23:44 +0800

    字体框架

--- js/maker.js
+++ js/maker.js
@@ -3,3 +3,12 @@ function paintImage(obj){
 	var ctx=c.getContext("2d");
 	ctx.drawImage(obj,10,10);
 }
+
+function finishText(){
+	var input=document.getElementById("text_input");
+	var text=input.value;
+	var c=document.getElementById("maker");
+	var ctx=c.getContext("2d");
+	ctx.font="20px Georgia";
+	ctx.fillText(text,10,50);
+}
