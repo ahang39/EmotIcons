0129d12ab1e0ccc135d8ccf3c0982aabeda44e7a
joyme <786185931@qq.com>
2016-05-05 13:32:25 +0800

    文字颜色属性修改

--- js/maker.js
+++ js/maker.js
@@ -1,4 +1,5 @@
 var canvas=null;
+var currentTextObj=null;//保存当前选中的文字对象
 mui.plusReady(function(){
 	canvas = new fabric.Canvas('maker');
 });
@@ -18,11 +19,55 @@ function paintImage(obj){
   });
 }
 
-function finishText(){
+function finishText(flag){
 	var input=document.getElementById("text_input");
 	var text=input.value;
-	var c=document.getElementById("maker");
-	var ctx=c.getContext("2d");
-	ctx.font="20px Georgia";
-	ctx.fillText(text,10,50);
+	var text = new fabric.Text(text, 
+	{
+		left: 100,
+		top: 100 ,
+		fontFamily: 'Comic Sans',
+		fontSize: 40,
+		fontWeight: 'normal',
+		shadow: 'green -5px -5px 3px',
+		fontStyle: 'italic',
+  		textBackgroundColor: 'rgb(0,200,0)'
+	});
+//	var text = new fabric.Text(text, 
+//	{
+//		left: 100,
+//		top: 100 ,
+//		fontFamily: 'Comic Sans',
+//		fontSize: 40,
+//		fontWeight: 'normal',
+//		shadow: 'green -5px -5px 3px',
+//		fontStyle: 'italic',
+//		stroke: '#c3bfbf',
+//		strokeWidth: 3,
+//		textAlign: 'right',
+//		lineHeight: 1,
+//		textBackgroundColor: 'rgb(0,200,0)'
+//	});
+	text.on('selected',function(){
+		document.getElementById("text_change").value=this.getText();
+		currentTextObj=this;
+	});
+	canvas.add(text);
+}
+
+function changeText(){
+	var changeText=document.getElementById("text_change").value
+	currentTextObj.setText(changeText);
+}
+function changeTextColor(obj){
+	console.log(obj.style.backgroundColor);
+	currentTextObj.setColor(obj.style.backgroundColor);
+}
+function changeTextBackgroundColor(obj){
+	console.log(obj.style.backgroundColor);
+	currentTextObj.setTextBackgroundColor(obj.style.backgroundColor);
+}
+function changeTextShadowColor(obj){
+	console.log(obj.style.backgroundColor);
+	currentTextObj.setTextBackgroundColor(obj.style.backgroundColor);
 }
